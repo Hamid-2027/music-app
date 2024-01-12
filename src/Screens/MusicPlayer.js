ae43ed4f2abcf78e85ca4607932e62a5f0638dd4
Hamid <hamidhussain2027@gmail.com>
2024-01-12 22:29:47 +0500

    added icon nav

--- src/Screens/MusicPlayer.js
+++ src/Screens/MusicPlayer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, View, StyleSheet} from "react-native";
-// import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon2 from 'react-native-vector-icons/AntDesign';
 import Icon from "react-native-vector-icons/Ionicons"
 
 const MusicPlayer =()=>{
@@ -8,10 +8,13 @@ const MusicPlayer =()=>{
 
     return(
     <View style={styles.container}>
+      <View style={styles.iconV}>
+
+        <Icon2 name ="like1" size={20} />
         <Icon name ="heart-outline" size={20} />
-        <Text>
-        MusicPlayer
-        </Text>
+        <Icon2 name ="like2" size={20} />
+      </View>
+     
         </View>
     );
 
@@ -20,9 +23,16 @@ const MusicPlayer =()=>{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+      justifyContent: 'center'
+    
     },
+    iconV:{
+      flexDirection:'row',
+      justifyContent:"space-around",
+      alignItems:'center',
+      borderWidth:1,
+
+    }
 
   });
   
